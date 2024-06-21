f89a4989f18603bcfb05ffd3175d80b7453d20bc
SamQueen <samqueenbusiness@gmail.com>
2024-06-21 15:35:21 -0700

    debug

--- src/routes/login.js
+++ src/routes/login.js
@@ -36,7 +36,7 @@ router.post('/login', async (req, res) => {
             // Set the token as a cookie
             res.cookie('access_token', accessToken, {
                 httpOnly: true,
-                secure: isProduction, // Ensure the cookie is sent over HTTPS in production
+                secure: false,//isProduction, // Ensure the cookie is sent over HTTPS in production
                 sameSite: isProduction ? 'None' : "Lax", // Set SameSite attribute
                 maxAge: 3600000, // 1 hour
                 domain: isProduction ? 'herokuapp.com' : 'localhost',
