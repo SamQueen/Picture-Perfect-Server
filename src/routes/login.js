697b7b6869b7908f4ad4247413ec29acb45c1eda
SamQueen <samqueenbusiness@gmail.com>
2024-06-21 03:05:55 -0700

    Update domain heroku

--- src/routes/login.js
+++ src/routes/login.js
@@ -39,7 +39,7 @@ router.post('/login', async (req, res) => {
                 secure: isProduction, // Ensure the cookie is sent over HTTPS in production
                 sameSite: isProduction ? 'None' : "Lax", // Set SameSite attribute
                 maxAge: 3600000, // 1 hour
-                domain: isProduction ? 'limitless-gorge-84901-46983064217d.herokuapp.com' : 'localhost',
+                domain: isProduction ? 'herokuapp.com' : 'localhost',
             });
 
             return res.status(200).json({ message: 'login successful' });
