e8f0821d954a1175c624550c7ae04cecc60d4459
SamQueen <samqueenbusiness@gmail.com>
2024-07-07 20:22:24 -0700

    Update cookie domain path

--- src/routes/login.js
+++ src/routes/login.js
@@ -40,7 +40,7 @@ router.post('/login', async (req, res) => {
                 sameSite: isProduction ? 'None' : "Lax", // Set SameSite attribute
                 maxAge: 3600000, // 1 hour
                 path: '/',
-                domain: isProduction ? '.pictureper.com' : undefined,
+                domain: isProduction ? 'api.pictureper.com' : undefined,
             });
 
             return res.status(200).json({ message: 'login successful' });
